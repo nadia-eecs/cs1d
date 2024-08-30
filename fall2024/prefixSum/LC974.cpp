9bd76b49b091cceceb5da75504b771b732316ef0
nadia-eecs <63071784+nadia-eecs@users.noreply.github.com>
2024-08-30 04:20:06 +0000

    updated mob programming course output

--- fall2024/prefixSum/LC974.cpp
+++ fall2024/prefixSum/LC974.cpp
@@ -1,19 +1,27 @@
 #include <vector>
+
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        vector<int> psum(nums.size() + 1) = {0};
-        vector<int> mod_counter(k) = {0};
+        vector<int> psum(nums.size() + 1,0);
+        vector<int> mod_p(psum.size(),0);
+        vector<int> mod_counter(k,0);
         int result = 0;
 
         psum = prefix_sum_generator(nums);
-
-        for(int i = 0; i < psum.size(); ++i){
-            mod_counter[i] = psum.at(i);
-            
+        mod_p = mod_generator(psum, k);
+        for(int i = 0; i < mod_p.size(); ++i){
+            mod_counter[mod_p[i]]++;
         }
+        // remove extra count for the origin that we added in psum
+        mod_counter[0]--;
 
-        
+        result = mod_counter[0]; // zeroth remainder is special initializing to the count
+        for(int i=0; i<k; i++) {
+            result += (mod_counter[i]*(mod_counter[i]-1)) / 2;
+        }
+    
+        return result;
     }
 
 private:
@@ -28,11 +36,18 @@ private:
 
     }
 
-    void mod_generator(vector<int>& prefix_sum, int k){
-        for(int i = 0; i < prefix_sum.size(); ++i){
-            prefix_sum[i] = 
+    vector<int> mod_generator(vector<int>& prefix_sum, int k){
+        
+        vector<int> mod_prefix(prefix_sum.size(),0);
+
+        for(int i = 0; i < prefix_sum.size(); ++i)
+        {
+            mod_prefix.at(i) = prefix_sum[i]%k;
+            if (mod_prefix[i] < 0) mod_prefix[i]+=k;
         }
+        return mod_prefix;
     }
+
 };
 
 /*
@@ -43,7 +58,7 @@ myArr = 42;
 
 /*
 To do: 
-1. create prefix sum array array 
+1. create prefix sum array array    
    - create helper function that returns a prefix sum array given the input vector nums
 2. Find the modulo of each individual item in prefix sum 
    - Check if the modulo is always positive
